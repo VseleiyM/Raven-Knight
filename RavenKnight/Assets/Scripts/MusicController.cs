afdc836acc446ff1c49e25ef1b7ad1acf7a2ad69
VseleiyM <goodvaselei2001@mail.ru>
2022-10-24 11:28:09 +0300

    New Zborka

---
+++ RavenKnight/Assets/Scripts/MusicController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    public UnityEngine.UI.Slider musicSlider;
+    public UnityEngine.UI.Slider soundSlider;
+    public UnityEngine.UI.Button newGameButton;
+    public UnityEngine.UI.Button loadButton;
+    public UnityEngine.UI.Button settingsButton;
+    public UnityEngine.UI.Button exitButton;
+    public UnityEngine.UI.Button menuButton;
+    
+    public AudioSource music;
+    public AudioSource click;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicSlider.value = music.volume;
+        soundSlider.value = click.volume;
+        musicSlider.onValueChanged.AddListener(OnMusicVolumeChanged);
+        soundSlider.onValueChanged.AddListener(OnSoundVolumeChanged);
+        
+        newGameButton.onClick.AddListener(Click);
+        loadButton.onClick.AddListener(Click);
+        settingsButton.onClick.AddListener(Click);
+        exitButton.onClick.AddListener(Click);
+        menuButton.onClick.AddListener(Click);
+    }
+    
+    private void OnMusicVolumeChanged(float newVolume)
+    {
+        music.volume = newVolume;
+    }
+    
+    private void OnSoundVolumeChanged(float newVolume)
+    {
+        click.volume = newVolume;
+    }
+    
+    private void Click()
+    {
+        click.Play();
+    }
+}
---
+++ RavenKnight/Assets/Scripts/SaveController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class SaveController : MonoBehaviour
+{
+    private SaveData _saveData = new SaveData();
+    private string _path;
+    
+
+    public UnityEngine.UI.InputField textField;
+    public UnityEngine.UI.Text testSave;
+    public UnityEngine.UI.Button saveButton; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        saveButton.onClick.AddListener(SaveData);
+        _path = Path.Combine(Application.dataPath, "Save.json");
+
+        if (File.Exists(_path))
+        {
+            _saveData = JsonUtility.FromJson<SaveData>(File.ReadAllText(_path));
+            testSave.text = _saveData.textData;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SaveData()
+    {
+        _saveData.textData = textField.text;
+        File.WriteAllText(_path, JsonUtility.ToJson(_saveData));
+    }
+}
+
+
+[Serializable]
+public class SaveData
+{
+    public string textData;
+}
