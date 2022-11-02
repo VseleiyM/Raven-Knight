6f8fcccdb661654125084edf65cd7956b9abd4d4
Nikita Botolov <nik-botolov@yandex.ru>
2022-11-03 02:48:21 +0300

    Pause, save configs

---
+++ RavenKnight/Assets/Scripts/UI/PauseUI.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseUI : MonoBehaviour
+{
+    public UnityEngine.UI.Button continueButton;
+    public UnityEngine.UI.Button settingsButton;
+    public UnityEngine.UI.Button backToPauseButton;
+    public UnityEngine.UI.Button backToMenuButton;
+    public UnityEngine.UI.Button exitButton;
+
+    public Gun gun;
+    public Test player;
+    public GameObject pauseUI;
+    public GameObject pauseObj;
+    public GameObject settingsObj;
+    public GameObject loadImage;
+
+    public static bool gameIsPaused;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1f;
+        gun.lookAtCursor = true;
+        continueButton.onClick.AddListener(ClosePause);
+        settingsButton.onClick.AddListener(OpenSettings);
+        backToPauseButton.onClick.AddListener(CloseSettings);
+        backToMenuButton.onClick.AddListener(BackToMainMenu);
+        exitButton.onClick.AddListener(ExitScene);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                ClosePause();
+            }
+            else
+            {
+                OpenPause();
+            }
+        }
+        
+    }
+
+    private void OpenPause()
+    {
+        pauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+        gun.lookAtCursor = false;
+    }
+    
+    private void ClosePause()
+    {
+        pauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+        gun.lookAtCursor = true;
+    }
+
+    private void OpenSettings()
+    {
+        pauseObj.SetActive(false);
+        settingsObj.SetActive(true);
+    }
+
+    private void CloseSettings()
+    {
+        settingsObj.SetActive(false);
+        pauseObj.SetActive(true);
+    }
+
+    private void BackToMainMenu()
+    {
+        SceneManager.LoadSceneAsync(0); 
+        loadImage.SetActive(true);
+    }
+    
+    private void ExitScene()
+    {
+        Application.Quit();
+    }
+}
---
+++ RavenKnight/Assets/Scripts/UI/SaveConfigsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class SaveConfigsController : MonoBehaviour
+{
+    private ConfigData _configData = new ConfigData();
+    private string _path;
+    
+    public UnityEngine.UI.Slider musicSlider;
+    public UnityEngine.UI.Slider soundSlider;
+    public UnityEngine.UI.Button applyChangesButton;
+    public AudioSource music;
+    public AudioSource click;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _path = Path.Combine(Application.dataPath, "Config.json");
+        applyChangesButton.onClick.AddListener(ApplyChanges);
+
+        
+        if (File.Exists(_path))
+        {
+            _configData = JsonUtility.FromJson<ConfigData>(File.ReadAllText(_path));
+            music.volume = _configData.musicValue;
+            click.volume = _configData.soundValue;
+        }
+        
+        musicSlider.value = music.volume;
+        soundSlider.value = click.volume;
+    }
+
+    // Update is called once per frame
+
+
+    private void ApplyChanges()
+    {
+        _configData.soundValue = click.volume;
+        _configData.musicValue = music.volume;
+        File.WriteAllText(_path, JsonUtility.ToJson(_configData));
+    }
+}
+
+[Serializable]
+public class ConfigData
+{
+    public float soundValue;
+    public float musicValue;
+}
