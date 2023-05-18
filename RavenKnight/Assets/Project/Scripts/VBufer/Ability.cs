df170de1d770e223e5ed43a2beede17f57249bc9
VseleiyM <goodvaselei2001@mail.ru>
2023-05-18 13:28:40 +0300

    Patch_dash_test_Abeility

---
+++ RavenKnight/Assets/Project/Scripts/VBufer/Ability.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Ability : MonoBehaviour
+{
+    [Header("Ability Settings")]
+    public string abilityName;
+    public float cooldownTime = 1f;
+    public bool IsActive { get; set; } = false;
+
+    private float _timePassed = 0f;
+
+    protected virtual void Start() { }
+
+    protected virtual void FixedUpdate()
+    {
+        if (IsActive)
+        {
+            _timePassed += Time.fixedDeltaTime;
+            if (_timePassed >= cooldownTime)
+            {
+                _timePassed = 0f;
+                IsActive = false;
+            }
+        }
+    }
+
+    public virtual void UseAbility()
+    {
+        if (!IsActive)
+        {
+            Debug.Log($"Using ability: {abilityName}");
+            
+
+            IsActive = true;
+        }
+    }
+}
+
