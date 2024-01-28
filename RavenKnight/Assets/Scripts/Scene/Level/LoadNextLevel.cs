e742fed99ff7dbadf7bcc53ce459ebc0f6218db8
Rvanare <prizrakt31@rambler.ru>
2024-01-28 18:01:15 +0900

    Patch
    Добавлено:
    - Анимация "Idle" для перехода на новый уровень
    - Добавлено ожидания нажатия кнопки "F", для перехода на новый уровень

--- RavenKnight/Assets/Scripts/Scene/Level/LoadNextLevel.cs
+++ RavenKnight/Assets/Scripts/Scene/Level/LoadNextLevel.cs
@@ -6,10 +6,41 @@ namespace Project
 {
     public class LoadNextLevel : MonoBehaviour
     {
+        [SerializeField] private Collider2D trigger;
+        [SerializeField] private GameObject notification;
+
+        private ContactPoint2D[] contacts;
+        private Collider2D target;
+
+        private void Awake()
+        {
+            notification.SetActive(false);
+            enabled = false;
+        }
+
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision.tag == "Player")
+            {
+                notification.SetActive(true);
+                enabled = true;
+                target = collision;
+            }
+        }
+
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.F))
+            {
                 GlobalEvents.SendLoadNextLevel();
+            }
+
+            
+            if (!trigger.IsTouching(target))
+            {
+                notification.SetActive(false);
+                enabled = false;
+            }
         }
     }
 }
